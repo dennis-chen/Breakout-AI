a7398c572bcfe9db5d3669f8154c794de7ab94b3
Filippos Lymperopoulos <filippos.lymperopoulos@students.olin.edu>
2014-10-09 13:08:57 -0400

    merged conflicts

--- AI.py
+++ AI.py
@@ -9,19 +9,12 @@ class AI():
 	# def get_next_move(self):
 	# 	return random.randint(-4,4)
 
-	def follow_the_ball(self):
-		distance = -self.player.rect.x + self.ball.rect.x
-		return int(distance/6)
+	# def follow_the_ball(self):
+	# 	distance = -self.player.rect.x + self.ball.rect.x
+	# 	return int(distance/6)
 
-    """AI for breakout game"""
-
-    def __init__(self,player,ball,blocks):
-        self.player = player
-        self.ball = ball
-        self.blocks = blocks
-
-    def get_random_next_move(self):
-        return random.randint(0,5)
+    # def get_random_next_move(self):
+    #     return random.randint(0,5)
 
     def follow_ball(self):
         dist_to_ball = self.ball.rect.x - self.player.rect.x
